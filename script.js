6783b20faecdd125a751b5b2ef2f53d16b066f34
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-20 00:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,12 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 for(let i=0;i<encodedStr.length;i++){
-	decodedArr+=lookup[encodedStr[i]]
+	let val=lookup[encodedStr[i]]
+	if(val===" "){
+		decodedArr+=" "
+	}else{
+		decodedArr+=val
+	}
 }
   return  decodedArr; //return decodedArr
 }
